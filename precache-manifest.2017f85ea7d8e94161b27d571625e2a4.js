42460e92f95f9604af0a663691c7b8b24dcc8199
Nataliia Korobkina <n.korobkina.888@gmail.com>
2019-02-01 14:48:18 -0600

    Updates

---
+++ precache-manifest.2017f85ea7d8e94161b27d571625e2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "f7102c78201b9bf32f5a",
+    "url": "/react-todo/static/js/main.f7102c78.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/react-todo/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "f7102c78201b9bf32f5a",
+    "url": "/react-todo/static/css/main.a169e1d3.chunk.css"
+  },
+  {
+    "revision": "d2ea3f8ff576511ff3f1061c261b2df6",
+    "url": "/react-todo/index.html"
+  }
+];
